fce2756ea7b4ec21b7ecad74f8fab1245b810601
qiang-cynthia <wang.wzx@outlook.com>
2019-03-27 00:19:12 +0800

    create the exponent.py

---
+++ exponent.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+base = float(input('Enter the base: '))
+exponent = float(input('Enter the exponent: '))
+
+# evaluate base to the power of exponent
+result = base ** exponent
+
+print('{} to the power of {} is: {}'.format(base, exponent, result))
