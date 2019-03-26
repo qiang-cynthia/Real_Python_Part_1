4de62708df7f88749216f3644c67f54bebebab08
qiang-cynthia <wang.wzx@outlook.com>
2019-03-26 22:45:26 +0800

    modify the replace_dict by converting its keys and values, and translate the same message

--- translate.py
+++ translate.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 sentense = str(input('Enter some text: '))
+sentense_1 = sentense
 
 # Bulid a replace_dict
 replace_dict = {'a':4, 'b':8, 'e':3, 'l':1, 'o':0, 's':5, 't':7}
+print(replace_dict)
 
 
 # Translate sentense by replacing key with value
@@ -12,6 +14,19 @@ for key,value in replace_dict.items():
 
 print(sentense)
 
+
+# Change the replace_dict by converting the keys and values
+replace_dict_1 = {}
+for key, value in replace_dict.items():
+    replace_dict_1[value] = key
+print(replace_dict_1)
+
+
+# Translate sentense_1 by replacing key with value
+for key,value in replace_dict.items():
+    sentense_1 = sentense_1.replace(str(key), str(value))
+
+print(sentense_1)
 
 # enter 'I like to eat eggs and spam.'
 # the result should be 'I 1ik3 70 347 3gg5 4nd 5p4m.'
